87a7dcbe3060acf736d07f01c6f4c036b7e4fd0f
Atharva <atharva.chalse547@gmail.com>
2023-11-16 23:40:23 +0530

    .

--- Code with Harry Python/Second Program.py
+++ Code with Harry Python/Second Program.py
@@ -1,1 +1,16 @@
-print("hello this is \n me ")
+print("hello this is \n me ") #printing hello world
+
+
+# This is a Comment
+'''
+
+ this is a multi line comment 
+'''
+
+"""
+
+MultiLine Comment
+
+"""
+
+print("this will \"this \"will "  )
---
+++ Code with Harry Python/more on print.py
@@ -0,0 +1,12 @@
+tableof1 = (1,2,3,4,5,6,7,8,9,10)
+
+print(*tableof1)
+
+# def tableof2(): 
+#     results = []
+#     for i in range(1,11):
+#         result = 2 * i
+#         results.append(result)  # Append each result to the list
+#     return results
+        
+# print(tableof2())
