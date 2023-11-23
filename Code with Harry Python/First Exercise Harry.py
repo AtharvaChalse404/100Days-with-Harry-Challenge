581189ce105b38b0918494e5ab046f051302a2dc
Atharva <atharva.chalse547@gmail.com>
2023-11-24 00:49:18 +0530

    .

---
+++ Code with Harry Python/First Exercise Harry.py
@@ -0,0 +1,50 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot Divide by Zero"
+
+def calculator():
+    print("Select Operation:")
+    print("1. ADD")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    num1 = int(input("Please Enter The First Int: "))
+    num2 = int(input("Please Enter The Second Int: "))
+
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+
+    else:
+        print("Invalid input")
+
+# Call the calculator function
+# 
+
+
+
+
+calculator()
+
