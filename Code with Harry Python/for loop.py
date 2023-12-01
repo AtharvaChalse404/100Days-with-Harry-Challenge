b7f61f9126bf5fc079adc7c44ec8d115c4cab5ee
Atharva <atharva.chalse547@gmail.com>
2023-12-02 02:27:11 +0530

    .

--- Code with Harry Python/for loop.py
+++ Code with Harry Python/for loop.py
@@ -15,3 +15,8 @@ list = [1,2,3,4,5,6,7,8,9]
 
 for i in list:
     print(i,end= '    ')
+
+print()
+
+for b in range(1, 20001,5):
+    print(b)
---
+++ Code with Harry Python/while loop.py
@@ -0,0 +1,14 @@
+i = int(input("please Enter a Base Value: "))
+while (i < 4):
+    i = int(input("please Enter a Number: "))
+    print(i)
+
+print (" While Loop Over")
+    
+    
+    
+# Always Include the i = i + 1 Inside The Block OF Code Nahi Kela kar Infinitly Loop Chalel
+
+
+
+#lihayla Lihla Baki While Loop Kay Smhjla Nahi Mla D:
