ef288e74d2db696ddbfdd3660517c45b62ced3b8
Atharva <atharva.chalse547@gmail.com>
2023-12-02 02:01:15 +0530

    .

--- Code with Harry Python/for loop.py
+++ Code with Harry Python/for loop.py
@@ -2,3 +2,16 @@ list = [1,2,3,4,5,6,7,8,9]
 
 for i in list:
     print(i)
+    
+    
+list = [1,2,3,4,5,6,7,8,9]
+
+for i in list:
+    print(i,end='')
+
+print()
+
+list = [1,2,3,4,5,6,7,8,9]
+
+for i in list:
+    print(i,end= '    ')
