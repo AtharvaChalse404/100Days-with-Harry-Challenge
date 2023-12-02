9ac2c083a3e673799d23320f1e760b39e39e8b97
Atharva <atharva.chalse547@gmail.com>
2023-12-03 01:35:48 +0530

    .

--- Code with Harry Python/function arguments.py
+++ Code with Harry Python/function arguments.py
@@ -1,6 +1,13 @@
-def average(a=9, b = 1):
-    print("The Avergage is ", (a+b)/2)
+# def average(a=9, b = 1):
+#     print("The Avergage is ", (a+b)/2)
     
-average()
+# average()
 
-average(b=9)
+# average(b=9)
+
+
+
+
+def name(fname , mname ="john", lname ="Whatson"):
+    print("Hello,",fname , mname , lname)
+name("amy","Agarwal","jain")
