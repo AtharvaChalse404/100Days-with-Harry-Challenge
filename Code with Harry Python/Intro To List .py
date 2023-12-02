d05bf8196e082e145b3eda67be1e254181df60bd
Atharva <atharva.chalse547@gmail.com>
2023-12-03 02:08:35 +0530

    .

---
+++ Code with Harry Python/Intro To List .py
@@ -0,0 +1,43 @@
+# l = [2,3,5,6]
+# # print(l)
+# # print(type(l))
+
+# for m in l:
+#     print(m)
+
+print()
+list = [1,2,3,4,5,6,7,8,9]
+
+# for i in list:
+#     print(i)
+    
+print(list[2])
+
+
+marks = [ 3,5,6,"Atharva", True , 6 , 7 ]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+# print(marks[4])
+
+# print()
+# print()
+
+# print(marks[-1])
+# print(marks[-2])
+# print(marks[-3])
+
+# print(marks[5-3])
+
+
+# if 7 in marks:print("yes")    
+
+# else: print("no")
+
+# if "array " in "Atharva":
+#     print("yes")
+
+print(marks[1:8:3])
