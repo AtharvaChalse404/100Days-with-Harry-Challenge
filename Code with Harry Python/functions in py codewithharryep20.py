349844a7d6025936bc88c6879236d21ba04a03a1
Atharva <atharva.chalse547@gmail.com>
2023-12-03 01:23:37 +0530

    .

---
+++ Code with Harry Python/functions in py codewithharryep20.py
@@ -0,0 +1,32 @@
+# def calculateGmean(a,b):
+#     mean = (a*b)/(a+b)
+#     print(mean)
+    
+
+
+a = 9 
+b = 8 
+# # gmean1 = (a*b)/(a+b)
+# # # print(gmean1)
+# calculateGmean(a,b)
+
+c = 8
+d= 7
+# # gmean2 = (c*d)/(c+d)
+# # print(gmean2)
+# calculateGmean(c,d)
+
+
+def isgreater(a,b):
+    if (a>b):
+        print("first number is greater or equal ")
+    else:
+        print("Second Number Is Greater Or Equal")
+
+isgreater(a,b)
+        
+
+isgreater(c,d)
+# Pass Means Ill Do It Later 
+def isLesser(a,b):
+    pass
