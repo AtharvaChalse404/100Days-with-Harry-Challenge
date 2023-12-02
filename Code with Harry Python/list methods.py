19f62cd7e0c771d3014bc441fde8958b8d5008db
Atharva <atharva.chalse547@gmail.com>
2023-12-03 02:27:24 +0530

    .

--- Code with Harry Python/list methods.py
+++ Code with Harry Python/list methods.py
@@ -1,5 +1,26 @@
-l = [1,2,4,6]
+l = [11,45,1,2,4,6]
 print(l)
-l.append(7)
-l.sort()
+# l.append(7)
+# l.sort()
+# print(l.index(1))
+# print(l)
+
+# print(l.count(1))
+
+# m = l.copy()
+# m[0] = 0
+
+# print(l)
+
+# print(m)
+
+
+
+l.insert(1,899)
 print(l)
+
+
+
+
+
+
