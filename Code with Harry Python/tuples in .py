5bfe14f9a394d6e84895590edffb1ae110b002ea
Atharva <atharva.chalse547@gmail.com>
2023-12-03 02:45:07 +0530

    .

---
+++ Code with Harry Python/tuples in .py
@@ -0,0 +1,28 @@
+tup = (1,2,76,342,32,"green", True)
+print(type(tup),tup)
+
+
+# tuples are unchaneable
+
+
+
+
+
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[3])
+
+
+# Everthing same as List But Just As i Said Tuple Ko Modify nahi Karu Skhakat
+
+
+
+# if 3421 in tup:
+#     print("yes")
+# else: print("no")
+
+# tuple la slice karayla ek navin tuple bnavla lagta ani tyat original la slice karayla lagta
+
+tup2 = tup[1:4]
+print(tup2)
