b19449592ef3516da2a854074395c60ebc385454
Atharva <atharva.chalse547@gmail.com>
2023-12-02 20:43:37 +0530

    .

--- Code with Harry Python/while loop.py
+++ Code with Harry Python/while loop.py
@@ -10,5 +10,5 @@ print (" While Loop Over")
 # Always Include the i = i + 1 Inside The Block OF Code Nahi Kela kar Infinitly Loop Chalel
 
 
-
+### While Loop Matlb Jbtk Ye Code Chlra h " While The Code Is Running" Ye Chlega ###
 #lihayla Lihla Baki While Loop Kay Smhjla Nahi Mla D:
