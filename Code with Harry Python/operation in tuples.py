1ca47d175f561220fb95e3bb52da6df3bbe85eb5
Atharva <atharva.chalse547@gmail.com>
2023-12-04 17:16:14 +0530

    .

--- Code with Harry Python/operation in tuples.py
+++ Code with Harry Python/operation in tuples.py
@@ -11,3 +11,12 @@
 tuple1 = (0,1,2,3,2,3,1,3,2)
 res = tuple1.count(3)
 print('count of 3 in tuple1 is :', res)
+
+
+countries = ("Spain", "Italy", "India", "England", "Germany")
+temp = list(countries)
+temp.append("Russia")       #add item 
+temp.pop(3)                 #remove item
+temp[3] = "Finland"         #change item
+countries = tuple(temp)
+print(countries)
