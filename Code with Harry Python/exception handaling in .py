a777ee4b7cc91d607b89203f78f8b6b876c07048
Atharva <atharva.chalse547@gmail.com>
2023-12-05 19:19:35 +0530

    .

--- Code with Harry Python/exception handaling in .py
+++ Code with Harry Python/exception handaling in .py
@@ -1,10 +1,22 @@
 a = input("Enter Numbers: ")
 
-try:
-    for i in range(1,11):
-        print(f"{int(a)} X {i} = {int(a)*i}"  )
-except Exception as e:
-    print(e)
+# try:
+#     for i in range(1,11):
+#         print(f"{int(a)} X {i} = {int(a)*i}"  )
+# except Exception as e:
+#     print(e)
     
     
-print("some imp line of code")
+# print("some imp line of code")
+
+
+
+try: 
+    num =int(input("Enter an Integer: "))
+    print(a[num])
+except ValueError:
+    print(' Value Error')
+except IndexError:
+    print('IndexError')
+except IndentationError:
+    print('IndentationError')
