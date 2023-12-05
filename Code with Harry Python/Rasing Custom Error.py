592721ce9f847ef80399d4706a9722f5dc10cc19
Atharva <atharva.chalse547@gmail.com>
2023-12-06 00:26:35 +0530

    .

--- Code with Harry Python/Rasing Custom Error.py
+++ Code with Harry Python/Rasing Custom Error.py
@@ -1,2 +1,4 @@
-a = int(input("Enter Any Value Between 5 and 9"))
+a = int(input("Enter Any Value Between 5 and 9: "))
 
+if (a< 5 or a > 9):
+    raise ValueError("Value Should be Between 5 and 9")
