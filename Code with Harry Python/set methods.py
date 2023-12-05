b0d215b2019fe085ed1f78fcb3488cca1c3038d4
Atharva <atharva.chalse547@gmail.com>
2023-12-05 18:17:42 +0530

    .

---
+++ Code with Harry Python/set methods.py
@@ -0,0 +1,19 @@
+s1 = {1,2,5,6}
+s2 = {3,6,7}
+
+# print(s1.union(s2))
+
+# s1.update(s2)
+# print(s1,s2)
+
+
+# s3 = s1.difference(s2)
+# print(s3)
+
+print(s1.isdisjoint(s2))
+
+# items = s1.pop()
+# print(items)
+
+# s1.clear()
+# print(s1)
