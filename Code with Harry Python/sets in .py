53ab0bba26dc5a85d7de919bd658c80129623b55
Atharva <atharva.chalse547@gmail.com>
2023-12-05 17:58:43 +0530

    .

---
+++ Code with Harry Python/sets in .py
@@ -0,0 +1,17 @@
+s = {2,4,2,5}
+print(s)
+
+#no Duplicate values 
+# The Order of the sets are random and hence they cant be accsessed using index nunbers
+
+# we use Curly Brackets For Sets 
+# and we seperatte them with Comma
+
+m = {}
+l = set(m)
+print(type(l))
+
+# the Syntax of Set And Dic Are Same {}
+# m = {} This is a Dic
+# s = {}
+#l = set{s} This is a Set 
