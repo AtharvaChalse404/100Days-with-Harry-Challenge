e17b30a94dd720ba6f0262d779422df481f1eb39
Atharva <atharva.chalse547@gmail.com>
2023-12-05 18:59:45 +0530

    .

---
+++ Code with Harry Python/for loop with else.py
@@ -0,0 +1,20 @@
+# for i in range(5):
+#     print(i)
+# else:
+#     print("no (i)")
+    
+# for i in range (6):
+#     print(i)
+#     if i == 4:
+#         break
+# else:
+#     print("no")
+
+i = 0
+while i < 7:
+    print(i)
+    i += 1
+    if i == 4:
+        break
+else:
+    print("no (i)")
