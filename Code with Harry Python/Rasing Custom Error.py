312f4c58e2afc989c52c38efbf480f8185a02f00
Atharva <atharva.chalse547@gmail.com>
2023-12-06 00:52:28 +0530

    .

--- Code with Harry Python/Rasing Custom Error.py
+++ Code with Harry Python/Rasing Custom Error.py
@@ -4,3 +4,15 @@ if (a< 5 or a > 9):
     raise ValueError("Value Should be Between 5 and 9")
 
 
+# Quick Quiz
+
+f = input('Please Enter QUIT')
+
+try:
+    if (f != 'QUIT'):
+        raise ValueError
+    else:
+        print('You Have Entered QUIT NIce')
+finally:
+    True
+
