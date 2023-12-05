49f34c1f3527cfcf652f1601e95403604187c8c8
Atharva <atharva.chalse547@gmail.com>
2023-12-05 18:31:57 +0530

    .

--- Code with Harry Python/dictionary.py
+++ Code with Harry Python/dictionary.py
@@ -1,5 +1,15 @@
 dic = {"key1":"value1","key2":"value2"}
 
-print(dic['key1'])
+# print(dic['key1'])
 
 # dictionaries where unordered before py 3.7
+
+
+# for key in dic.keys():
+#     print(key)
+
+
+print(dic.items())
+
+for key , value in dic.items():
+    print(f"The Value Corresponding to the key {key} is {value}")
