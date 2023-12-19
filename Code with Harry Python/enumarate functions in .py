2b4a2eba392a6ca97a293bee61101c0844cff15c
Atharva <atharva.chalse547@gmail.com>
2023-12-19 21:57:53 +0530

    .

---
+++ Code with Harry Python/enumarate functions in .py
@@ -0,0 +1,13 @@
+marks = [12,56,32,98,12,45,1,4]
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if (index == 3):
+#         print('I am Awesome')
+#     index +=1
+    
+    # index first in order and then value
+for index,mark in enumerate(marks):
+    print(mark)
+    if (index == 3):
+        print('I am Awesome')
