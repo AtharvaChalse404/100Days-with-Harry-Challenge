e3a87cadb01423daab3d1a2d2e11c10939471eec
Atharva <atharva.chalse547@gmail.com>
2023-12-19 21:51:10 +0530

    .

--- Code with Harry Python/shorthandifelse.py
+++ Code with Harry Python/shorthandifelse.py
@@ -4,11 +4,3 @@ b = 3303
 print("a") if a > b else print("=") if a == b else print("B")
 
 
-if a > b:
-    if a == b:
-        pass
-    else:
-        print("B")
-    print("a")
-else:
-    print("=")
