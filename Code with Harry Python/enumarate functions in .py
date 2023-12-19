f44e82767cc314df0862e472bd855c50c37bf233
Atharva <atharva.chalse547@gmail.com>
2023-12-19 22:09:51 +0530

    .

--- Code with Harry Python/enumarate functions in .py
+++ Code with Harry Python/enumarate functions in .py
@@ -7,7 +7,18 @@ marks = [12,56,32,98,12,45,1,4]
 #     index +=1
     
     # index first in order and then value
-for index,mark in enumerate(marks):
+    
+    
+    
+# for index,mark in enumerate(marks):
+#     print(mark)
+#     if (index == 3):
+#         print('I am Awesome')
+
+# index always start at 0 but u can change it 
+
+
+for index,mark in enumerate(marks, start=1):
     print(mark)
     if (index == 3):
         print('I am Awesome')
