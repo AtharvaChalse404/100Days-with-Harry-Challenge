dde7a9107024b7372d0bd40660e35ae4c24eb4c6
Atharva <atharva.chalse547@gmail.com>
2023-12-29 00:32:30 +0530

    .

--- Code with Harry Python/file io .py
+++ Code with Harry Python/file io .py
@@ -1,5 +1,15 @@
-f = open('myfile.txt','r')
+f = open('myfile.txt','w')
 
 # R For Reading
 # W For Writing
 # A For Appending
+
+
+
+# text = f.read()
+# print(text)
+# f.close()
+
+text = f.write()
+print(text)
+f.close()
