3d31f55c7d880debfa14a86455e255be9478d14b
Atharva <atharva.chalse547@gmail.com>
2023-12-30 00:07:03 +0530

    .

--- Code with Harry Python/lamba.py
+++ Code with Harry Python/lamba.py
@@ -10,3 +10,14 @@ double = lambda x: x*2
 cube = lambda y: y*y*y
 
 print(cube(5))
+
+avg = lambda x,y,z: (x+y+z) / 3
+
+print(avg(3 ,5 , 10))
+
+def appl(fx,value):
+    return 6+ fx(value)
+
+print(appl(cube,2))
+
+print(appl(lambda x:x*x*x,2))
