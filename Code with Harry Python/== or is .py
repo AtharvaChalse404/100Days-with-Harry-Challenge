5dfca37989ae5bece4edf18ff615b2a4af61271f
Atharva <143740199+AtharvaChalse404@users.noreply.github.com>
2024-01-14 00:21:22 +0530

    .

---
+++ Code with Harry Python/== or is .py
@@ -0,0 +1,14 @@
+a = 4
+b = 4
+
+print(a is b) # Exact Location of Object in memory
+print(a == b) # Value
+
+# a = 4
+# b = 4
+
+
+# now these Two Share The Same Memory  Location 
+# IS Compares Identitty
+
+# = Compares Value
