e8be6088d1497622dd1095254b51ab2be54d564b
Atharva <143740199+AtharvaChalse404@users.noreply.github.com>
2024-01-14 18:53:53 +0530

    .

---
+++ Code with Harry Python/Classes And Objects.py
@@ -0,0 +1,18 @@
+class person:
+    name = "harry"
+    occupation = "software dev"
+    networth = 10
+    
+    def info(self):  
+        print(f"{self.name} is a {self.occupation}")
+
+a = person()
+a.name = "Atharva"
+a.occupation = "Accountant"
+# print(a.name, a.occupation)
+a.info()  
+
+
+# A Class Is a Blueprint or a Template
+# Works as a PlaceHolder
+
