f455d10e4c7e0f07a76efb389f5b863c7bb2ce8b
Atharva <143740199+AtharvaChalse404@users.noreply.github.com>
2024-01-14 18:34:46 +0530

    .

---
+++ Code with Harry Python/Classes And Objects.py
@@ -0,0 +1,10 @@
+class person:
+    name ="Harry"
+    occupation = "Software Dev"
+    networth = 10
+    
+    
+a = person()
+a.name ="Shubman"
+a.occupation = "Accountant"
+print(a.name,a.occupation)
