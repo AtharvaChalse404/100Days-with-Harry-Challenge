58e4316970d74907eb710ab5464b76f608e74b4b
Atharva <143740199+AtharvaChalse404@users.noreply.github.com>
2024-01-24 00:33:17 +0530

    .

---
+++ Decorators.py
@@ -0,0 +1,16 @@
+def greet(fx):
+    def mfx():
+        print("Good Morning")
+        fx()
+        print(" Thanks for Using This Function")
+    return mfx
+
+@greet
+
+def hello():
+    print("Hello World")
+    
+def add (a,b):
+    print(a+b)
+    
+hello()
