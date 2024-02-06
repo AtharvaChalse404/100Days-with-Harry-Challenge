86014645be087ac10f8252f38d514ef27ad0c179
Atharva <143740199+AtharvaChalse404@users.noreply.github.com>
2024-02-07 00:17:03 +0530

    .

--- access modifers.py
+++ access modifers.py
@@ -1,9 +1,18 @@
-class employee:
-    pass
-
-a = employee()
-
-a.emp1 = 5
 
 # Public , Private , Encrypted Access Modifiers
 # all Variables are by Default Public
+
+class employee:
+    def __init__(self):
+        self.__name = "Harry"
+        
+
+a = employee()
+# print(a.__name) Cannot Be Accessed Directly
+
+
+
+# The Above is a Private Access  It is Indicated Using "   __         "
+print(a._employee__name) # can be accessed Indirectly
+
+
