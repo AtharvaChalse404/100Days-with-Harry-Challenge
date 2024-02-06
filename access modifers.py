8b2c45c84b9966fc14d8a06332471c15818640a7
Atharva <143740199+AtharvaChalse404@users.noreply.github.com>
2024-02-07 00:19:15 +0530

    .

--- access modifers.py
+++ access modifers.py
@@ -1,5 +1,5 @@
-
-# Public , Private , Encrypted Access Modifiers
+# This Is For OOPS ON GLOBAL NOT ONLY PYTHON , PYTHON DOESNT ENFORCE IT 
+# Public , Private , Encrypted/Protected Access Modifiers
 # all Variables are by Default Public
 
 class employee:
